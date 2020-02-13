54a269548f6e153b74dee93ec62793e014417b6c
Rob Quinn <Rob.M.Quinn@gmail.com>
2020-02-13 17:49:03 -0500

    wip

---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Barnyard", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Black and Blue", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("HAMburger", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Greek Burger", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Impossible", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Jersey Special", 0);
+
+select * from burgers;
---
+++ models/burger_0ld.js
@@ -0,0 +1,19 @@
+// Dependencies
+// =============================================================
+
+// This may be confusing but here Sequelize (capital) references the standard library
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references our connection to the DB.
+var sequelize = require("../config/connection.js");
+
+// Creates a "Chirp" model that matches up with DB
+var Chirp = sequelize.define("burger", {
+  burger_name: Sequelize.STRING,
+  devoured: Sequelize.BOOLEAN
+});
+
+// Syncs with DB
+Chirp.sync();
+
+// Makes the Chirp Model available for other files (will also create a table)
+module.exports = Chirp;
