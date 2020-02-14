4c11cd354c134ef2b9839e23eda0b897f7ca8e31
Rob Quinn <Rob.M.Quinn@gmail.com>
2020-02-13 21:18:24 -0500

    updated port

--- config/connection.js
+++ config/connection.js
@@ -16,7 +16,7 @@ else {
   //MySQL password is passed into connection.js from the .env file using the dotenv npm package.
   var connection = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: "3306",
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
